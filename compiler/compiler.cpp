0aa9d8bb44a888def433f946784fd585eeee552f
Xlang_Xiaokang00010 <3134714226@qq.com>
2021-02-01 11:29:38 +0800

    fix function call statement and more bugs

--- compiler/compiler.cpp
+++ compiler/compiler.cpp
@@ -106,12 +106,15 @@ class TypeName{
             objects[i->first] = i->second;
         }
     }
-    TypeName(){}
+    TypeName(){
+        this->size = 0;
+    }
     TypeName(string name,VarType type){
         this->name = name;
         this->type = type;
         if(type == __BASIC_8BYTE) this->size = 8;
-        if(type == __BASIC_1BYTE) this->size = 1;
+        else if(type == __BASIC_1BYTE) this->size = 1;
+        else this->size = 0;
     }
     void InsertToObject(string s,TypeName t){
         size += t.size;
@@ -211,9 +214,9 @@ map<string,function_block> function_table;
 string getFunctionRealName(ASTree a,TypeName& this_scope){
     if(!ASTree_APIs::MemberExpression::hasFunctionCallStatement(a))  return ""; // It's a stupid fix.But,It's work now.
     else{
-        if(a.node[1].nodeT == FunctionCallStatement) return "_@" + this_scope.name  + "_" + a.node[1].this_node.str;
+        if(a.node[1].nodeT == FunctionCallStatement) return "_@" + this_scope.findObject(a.node[0]).name  + "_" + a.node[1].this_node.str;
         else{
-            return getFunctionRealName(a.node[1],this_scope[a.node[1].node[0].this_node.str]);
+            return getFunctionRealName(a.node[1],this_scope[a.node[0].this_node.str]);
         }
     }
 }
@@ -222,8 +225,8 @@ string getFunctionRealName(ASTree a,TypeName& this_scope){
 string getFunctionRealName(ASTree a){
     if(a.nodeT == FunctionCallStatement) return "_"+a.this_node.str;
     if(a.nodeT != ExpressionStatement) return "";
-    if(type_pool.count((a.node[0].this_node.str)) == 0) return "";
-    return getFunctionRealName(a,type_pool[guessType(a.node[0])]);
+    if(type_pool.count(guessType(a.node[0])) == 0) return "";
+    return getFunctionRealName(a.node[1],type_pool[guessType(a.node[0])]);
 }
 
 ASTree getFunctionCallArgs(ASTree ast){
@@ -281,19 +284,19 @@ string guessType(ASTree ast){
             throw CompileError("Unknown Constant:" + TOKEN_VALUE_DESCRIPTION[ast.this_node.type]);
             break;
         }
+    }else if(ASTree_APIs::MemberExpression::hasFunctionCallStatement(ast)){
+        return funcnameInTab(getFunctionRealName(ast)).substr(getFunctionRealName(ast).length()+1);
     }else if(ast.nodeT == ExpressionStatement){
-        if(ast.nodeT == TOK_DOT){
+        if(ast.this_node.type == TOK_DOT){
             if(symbol_table.count(ast.node[0].this_node.str)) return type_pool[symbol_table[ast.node[0].this_node.str]._Typename].findObject(ast.node[1]).name;
-            else if(global_symbol_table.count(ast.node[0].this_node.str)) return type_pool[symbol_table[ast.node[0].this_node.str]._Typename].findObject(ast.node[1]).name;
+            else if(global_symbol_table.count(ast.node[0].this_node.str)) return type_pool[global_symbol_table[ast.node[0].this_node.str]._Typename].findObject(ast.node[1]).name;
             else throw CompileError(ast.node[0].this_node.str + " doesn't exist");
-        }else if(ast.nodeT == TOK_COLON){
+        }else if(ast.this_node.type == TOK_COLON){
             return ast.node[0].this_node.str; // 送 业 绩
         }else{
             if(guessType(ast.node[0]) == "string" || guessType(ast.node[1]) == "string") return "string";
             return guessType(ast.node[0]);
         }
-    }else if(ast.nodeT == FunctionCallStatement){
-        return funcnameInTab(getFunctionRealName(ast)).substr(getFunctionRealName(ast).length());
     }
 }
 
@@ -339,7 +342,7 @@ ASMBlock dumpToAsm(ASTree ast,int mode = false/*default is cast mode(0),but in g
             return ASMBlock().genCommand("mov").genArg("reg" + to_string(getLastUsingRegId())).genArg(to_string(ConstPool_Apis::Insert(cp,(char*)ast.this_node.str.c_str(),ast.this_node.str.size()))).push();
         }
         if(symbol_table.find(ast.this_node.str) != symbol_table.end()){
-            return ASMBlock().genCommand("mov").genArg("reg" + to_string(getLastUsingRegId())).genArg(to_string(symbol_table[ast.this_node.str].frame_position)).genCommand("sub").genArg("reg"+to_string(getLastUsingRegId())).genArg("regfp").push();
+            return ASMBlock().genCommand("mov").genArg("reg" + to_string(getLastUsingRegId())).genArg(to_string(symbol_table[ast.this_node.str].frame_position)).genCommand("mov").genArg("reg"+to_string(getLastUsingRegId())).genArg("stackbase").genCommand("sub").genArg("reg"+to_string(getLastUsingRegId())).genArg("regfp").push();
         }
         if(global_symbol_table.find(ast.this_node.str) != global_symbol_table.end()){
             return ASMBlock().genCommand("mov").genArg("reg" + to_string(getLastUsingRegId())).genArg(to_string(global_symbol_table[ast.this_node.str].frame_position)).push();
@@ -360,16 +363,23 @@ ASMBlock dumpToAsm(ASTree ast,int mode = false/*default is cast mode(0),but in g
                 asb.genCommand("push").genArg("[" + to_string(getLastUsingRegId()) + "]").genArg(to_string(getMemberSize(args.node[i]))).push();
             }
         }
-        return asb.genCommand("call").genArg(funcnameInTab(func_name)).push();
+        cout << "\033[32mGuess Result:\033[0m" << guessType(ast) << " " << getFunctionRealName(ast) << endl;
+        asb.genCommand("call").genArg(funcnameInTab(func_name)).\
+        genCommand("mov").genArg("reg" + to_string(getLastUsingRegId())).genArg("stackbase").\
+        genCommand("sub").genArg("reg" + to_string(getLastUsingRegId())).genArg("regfp").\
+        genCommand("sub").genArg("reg" + to_string(getLastUsingRegId())).genArg("regsp").\
+        genCommand("add").genArg("reg" + to_string(getLastUsingRegId())).genArg(to_string(getMemberSize(ast))).push();
+        sp += getMemberSize(ast);
+        return asb;
     }
     if(ast.nodeT == ExpressionStatement){
         if(ast.this_node.type == TOK_PLUS || ast.this_node.type == TOK_MINUS || ast.this_node.type == TOK_MULT || ast.this_node.type == TOK_DIV || ast.this_node.type == TOK_2EQUAL || ast.this_node.type == TOK_MAXEQUAL || ast.this_node.type == TOK_MINEQUAL || ast.this_node.type == TOK_MAX || ast.this_node.type == TOK_MIN){
             ASMBlock ab;
             ab += dumpToAsm(ast.node[0]);
-            if(ast.node[0].nodeT == FunctionCallStatement || ASTree_APIs::MemberExpression::hasFunctionCallStatement(ast.node[0])) ab.genCommand("pop").genArg("reg" + to_string(getLastUsingRegId()));
+            //if(ast.node[0].nodeT == FunctionCallStatement || ASTree_APIs::MemberExpression::hasFunctionCallStatement(ast.node[0])) ab.genCommand("").genArg("reg" + to_string(getLastUsingRegId()));
             RegState[getLastUsingRegId()] = true;
             ab += dumpToAsm(ast.node[1]);
-            if(ast.node[1].nodeT == FunctionCallStatement || ASTree_APIs::MemberExpression::hasFunctionCallStatement(ast.node[1])) ab.genCommand("pop").genArg("reg" + to_string(getLastUsingRegId()));
+            if(ast.node[1].nodeT == FunctionCallStatement || ASTree_APIs::MemberExpression::hasFunctionCallStatement(ast.node[1])) sp -= getMemberSize(ast.node[1]);
             RegState[getLastUsingRegId()] = true;
             if(ast.this_node.type == TOK_PLUS) ab.genCommand("add");
             else if(ast.this_node.type == TOK_MINUS) ab.genCommand("sub");
@@ -383,11 +393,14 @@ ASMBlock dumpToAsm(ASTree ast,int mode = false/*default is cast mode(0),but in g
             // Xlang变量的dumpASM会传地址
             // 加减乘除默认8byte运算
             if(ast.node[0].this_node.type == TOK_INTEGER || ast.node[0].this_node.type == TOK_DOUBLE || ast.node[0].this_node.type == TOK_CHARTER ) ab.genArg("reg" + to_string(getLastUsingRegId() - 2));
-            else if(ast.node[0].nodeT == FunctionCallStatement || ASTree_APIs::MemberExpression::hasFunctionCallStatement(ast.node[0])) ab.genArg("reg" + to_string(getLastUsingRegId() - 2));
+            else if(ast.node[0].nodeT == FunctionCallStatement || ASTree_APIs::MemberExpression::hasFunctionCallStatement(ast.node[0])){
+                ab.genArg("[reg" + to_string(getLastUsingRegId() - 2) + "]");
+                sp -= getMemberSize(ast.node[0]);
+            }
             else if(getMemberType(ast.node[0]) == __BASIC_8BYTE || getMemberType(ast.node[0]) == __BASIC_1BYTE) ab.genArg("[reg" + to_string(getLastUsingRegId() - 2) + "]");
             else throw CompileError("TypeError: 加减乘除以及逻辑运算仅限于基础类型,除非你想让虚拟机崩掉.\nBasic operator and boolean expression only support basic types,if you want to let the vm crash then don't do it.");
             if(ast.node[1].this_node.type == TOK_INTEGER || ast.node[1].this_node.type == TOK_DOUBLE || ast.node[1].this_node.type == TOK_CHARTER) ab.genArg("reg" + to_string(getLastUsingRegId() - 1));
-            else if(ast.node[1].nodeT == FunctionCallStatement || ASTree_APIs::MemberExpression::hasFunctionCallStatement(ast.node[1])) ab.genArg("reg" + to_string(getLastUsingRegId() - 1));
+            else if(ast.node[1].nodeT == FunctionCallStatement || ASTree_APIs::MemberExpression::hasFunctionCallStatement(ast.node[1])) ab.genArg("[reg" + to_string(getLastUsingRegId() - 1) + "]");
             else if(getMemberType(ast.node[1]) == __BASIC_8BYTE || getMemberType(ast.node[1]) == __BASIC_1BYTE) ab.genArg("[reg" + to_string(getLastUsingRegId() - 1) + "]");
             else throw CompileError("TypeError: 加减乘除以及逻辑运算仅限于基础类型,除非你想让虚拟机崩掉.\nBasic operator and boolean expression only support basic types,if you want to let the vm crash then don't do it.");
             RegState[getLastUsingRegId() - 1] = false;
@@ -397,7 +410,7 @@ ASMBlock dumpToAsm(ASTree ast,int mode = false/*default is cast mode(0),but in g
         if(ast.this_node.type == TOK_DOT){
             // address only
             int fp_offset = type_pool[symbol_table[ast.node[0].this_node.str]._Typename].getOffset(ast.node[1],symbol_table[ast.node[0].this_node.str].frame_position);
-            return ASMBlock().genCommand("mov").genArg("reg" + getLastUsingRegId()).genArg(to_string(fp_offset)).genCommand("sub").genArg("reg" + to_string(getLastUsingRegId())).genArg("regfp").push();
+            return ASMBlock().genCommand("mov").genArg("reg" + getLastUsingRegId()).genArg(to_string(fp_offset)).genCommand("mov").genArg("reg"+to_string(getLastUsingRegId())).genArg("stackbase").genCommand("sub").genArg("reg" + to_string(getLastUsingRegId())).genArg("regfp").push();
         }
         if(ast.this_node.type == TOK_EQUAL){
             ASMBlock asb;
@@ -440,7 +453,10 @@ ASMBlock dumpToAsm(ASTree ast,int mode = false/*default is cast mode(0),but in g
                 TypeName this_type = type_pool[contents.node[i].this_node.str];
                 ASTree& temp_ast = contents.node[i];
                 for(int j = 0;j < temp_ast.node.size();j++){
-                    if(temp_ast.node[j].nodeT == Id) t.InsertToObject(temp_ast.node[j].this_node.str,this_type);
+                    if(temp_ast.node[j].nodeT == Id){
+                        t.InsertToObject(temp_ast.node[j].this_node.str,this_type);
+                        
+                    }
                     else throw CompileError("Compiler doesn't support init value now"); // TODO: Add init value support
                 }
             }
@@ -492,6 +508,7 @@ ASMBlock dumpToAsm(ASTree ast,int mode = false/*default is cast mode(0),but in g
                     else realarg0 = "[" + realarg0 + "]";
                     symbol_table[ast.node[i].node[0].this_node.str] = Symbol(typen.name);
                     asb.genCommand("push").genArg(realarg0).genArg(to_string(typen.size));
+                    sp += typen.size;
                 }
             }
             return asb.push();
@@ -589,6 +606,15 @@ ASMBlock dumpToAsm(ASTree ast,int mode = false/*default is cast mode(0),but in g
         }
         if(ast.this_node.str == "continue"){
             return ASMBlock().genCommand("_$fakecommand_loop_continue").push();
+        }
+        if(ast.this_node.str == "return"){
+            ASMBlock asb;
+            asb.genCommand("pop_frame").push();
+            asb += dumpToAsm(ast.node[0]);
+            string realarg0 = "reg" + to_string(getLastUsingRegId());
+            if(ast.node[0].this_node.type == TOK_INTEGER || ast.node[0].this_node.type == TOK_DOUBLE || ast.node[0].this_node.type == TOK_CHARTER ) /*do nothing*/;
+            else realarg0 = "[" + realarg0 + "]";
+            return asb.genCommand("push").genArg(realarg0).genArg(to_string(getMemberSize(ast.node[0]))).push();
         }
         throw CompileError("Unknown Command: " + ast.this_node.str);
     }
